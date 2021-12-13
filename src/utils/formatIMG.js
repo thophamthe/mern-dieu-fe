605388ba1c1adfbe8cd8f6fc720a00fc74323a70
Phạm Thế Tho <86980956+thophamthe@users.noreply.github.com>
2021-12-13 12:17:39 +0000

    update host be

--- src/utils/formatIMG.js
+++ src/utils/formatIMG.js
@@ -1,7 +1,7 @@
 export function formatIMG(url){
     if(url){
         
-        return "https://mern-dieu-be.herokuapp.com/"+url
+        return "https://main.d3q6gzmjweguto.amplifyapp.com/"+url
     }else{
         return "https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png"
     }
