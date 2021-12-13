5d659bc345168c819268ec792df355c2bac82398
Phạm Thế Tho <86980956+thophamthe@users.noreply.github.com>
2021-12-13 12:17:02 +0000

    update hostbe

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const axiosClient= axios.create({
-    baseURL:"https://mern-dieu-be.herokuapp.com/api",
+    baseURL:"https://main.d3q6gzmjweguto.amplifyapp.com/api",
     headers:{
         "Content-type": "application/json; charset=UTF-8",
         "token": localStorage.getItem('token')
