fdc2f8dd6825f7ffb6683c4cb4e64ef9c1e27985
Phạm Thế Tho <86980956+thophamthe@users.noreply.github.com>
2021-12-16 19:12:44 +0700

    Update formatIMG.js

--- src/utils/formatIMG.js
+++ src/utils/formatIMG.js
@@ -1,7 +1,6 @@
 export function formatIMG(url){
     if(url){
-        
-        return "https://mern-dieu-be-production.up.railway.app//"+url
+        return "https://mern-dieu-be-production.up.railway.app/"+url
     }else{
         return "https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png"
     }
